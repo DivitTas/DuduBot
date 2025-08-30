1833b97a1231d1e7065bfbf2606c13df1692b81b
DivitTas <divit.nt@gmail.com>
2025-08-30 07:42:18 +0530

    add __init__.py files

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
-
-bot = commands.Bot(command_prefix="$", intents=discord.Intents.default())
-
+intents=discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="$", intents=intents)
 @bot.event
 async def on_ready():
     print("Read to MEOW")
@@ -12,4 +12,5 @@ async def load_cogs():
 
 def run_bot(token):
     bot.run(token)
+    load_cogs()
 
